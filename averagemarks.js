f071176302c9e2d8979aa142a3edfa375cbdb9e1
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-03-24 17:17:53 +0300

    feat: add js files

--- averagemarks.js
+++ averagemarks.js
@@ -1,9 +1,30 @@
 console.log('Outputting in the console');
 
-const students = [
-    { name:"Jane", marks: [80]},
-    { name:"Ciara", marks: [77]},
-    { name:"Smith", marks: [88]},
-    { name:"Thomas", marks: [95]},
-    { name:"Shelby", marks: [68]}
-]
+let students = [
+    {name:"Jane", mark: 80},
+    {name:"Ciara", mark: 77},
+    {name:"Smith", mark: 88},
+    {name:"Thomas", mark: 95},
+    {name:"Shelby", mark: 68}
+];
+
+let total = 0;
+for (let i = 0; i < students.length; i++) {
+    total += students[i].mark;
+}
+
+let average = total / students.length;
+console.log(average);
+
+if (average < 60) {
+    console.log("Grade: F");
+} else if (average < 70) {
+    console.log("Grade: D");
+} else if (average < 80) {
+    console.log("Grade: C");
+} else if (average < 90) {
+    console.log("Grade: B");
+} else if (average <= 100) {
+    console.log("Grade: A");
+};
+
---
+++ diff.js
@@ -0,0 +1,10 @@
+let num = 12;
+let result; 
+
+if (num > 13) {
+    result = (num -13)*2;
+} else {
+    result = 13 - num;
+}
+
+console.log(result);

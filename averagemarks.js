bc6736f03fa4af01dee2e3704107cf6aca600ed3
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-03-24 16:10:36 +0300

    feat: add average marks js file

---
+++ averagemarks.js
@@ -0,0 +1,9 @@
+console.log('Outputting in the console');
+
+const students = [
+    { name:"Jane", marks: [80]},
+    { name:"Ciara", marks: [77]},
+    { name:"Smith", marks: [88]},
+    { name:"Thomas", marks: [95]},
+    { name:"Shelby", marks: [68]}
+]

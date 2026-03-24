0b537623a9c9be4b35e7729320e0e885866de089
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-03-24 15:56:25 +0300

    feat: add js files

--- largestinteger.js
+++ largestinteger.js
@@ -1,1 +1,10 @@
 console.log('Outputting in the console');
+
+let num1 = 10;
+let num2 = 20;
+
+if (num1 > num2) {
+    console.log("num1 is larger than num2");
+} else {
+    console.log("num2 is larger than num1");
+}

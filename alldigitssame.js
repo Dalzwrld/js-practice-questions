feb02648c3ea7f8414c47c5e7fb3d1a9dded24b1
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-03-25 09:09:20 +0300

    feat: add js files

---
+++ alldigitssame.js
@@ -0,0 +1,11 @@
+let num = 1111;
+
+let numStr = num.toString();
+let allSame = true;
+
+for (let i = 0; i < numStr.length; i++) {
+    if (numStr[i] !== numStr[0]) 
+    allSame = false;
+} 
+
+console.log(allSame);
